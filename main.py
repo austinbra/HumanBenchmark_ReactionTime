91b1824f79a975d6317f3ce4897ec25f06dba4ac
austinbra <129190342+austinbra@users.noreply.github.com>
2024-01-01 23:43:15 -0600

    Update main.py
    kind of complete

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from PIL import ImageGrab
+import time
+import pyautogui
+
+def checkColor(x, y):
+    pixel_color = pyautogui.pixel(x, y)
+    return pixel_color
+
+
+while True:
+    if checkColor(842,614) == (75,219,106):
+        pyautogui.click(x=842, y=614)
 
