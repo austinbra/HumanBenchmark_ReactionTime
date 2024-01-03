b0fef8673b84267348154ca50dd5561b6fc6ab32
austinbra <129190342+austinbra@users.noreply.github.com>
2024-01-02 23:53:00 -0600

    final

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from PIL import ImageGrab
 import time
 import pyautogui
+import keyboard
+import sys
+
 
 def checkColor(x, y):
+    #finds the rgb values at the coordinate
     pixel_color = pyautogui.pixel(x, y)
     return pixel_color
 
+def get_xy():
+    # Get the screen width and height
+    screen_width, screen_height = pyautogui.size()
+    # Calculate the coordinates of the point halfway down the screen a little to the left
+    center_x = screen_width // 3
+    center_y = screen_height // 2
+    return center_x, center_y
 
+print("Press the Ctrl key to run script")
 while True:
-    if checkColor(842,614) == (75,219,106):
-        pyautogui.click(x=842, y=614)
-
+    if keyboard.is_pressed('ctrl'):
+        print("Running Code")
+        while True:
+            x,y = get_xy()
+            if checkColor(x,y) == (75,219,106):
+                pyautogui.click(x, y)    
+#script only runs after the ctrl key is pressed
+#to stop script click on terminal and press 'Ctrl + c'
